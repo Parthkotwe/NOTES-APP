966a9eda90c36c57e7cd0f452cc0e166a7bcbab2
Parthkotwe <parthkotwe73@gmail.com>
2025-10-27 17:16:02 +0530

    Added vercel.json and redirect route to login

--- frontend/notes-app/src/App.jsx
+++ frontend/notes-app/src/App.jsx
@@ -1,22 +1,23 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
-import './index.css'
-import Home from './pages/Home/Home'
-import Login from './pages/Login/login'
-import Signup from './pages/Signup/signup'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login/Login";
+import Signup from "./pages/Signup/Signup";
+import Dashboard from "./pages/Dashboard/Dashboard";
 
-const App = () => {
+function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Navigate to='/signup' replace />} />
+        <Route path="/" element={<Navigate to="/login" />} />
+        
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
 
-        <Route path='/dashboard' element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/signup' element={<Signup />} />
+        <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
